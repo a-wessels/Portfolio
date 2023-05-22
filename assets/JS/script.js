7dcae3ea22b410f0aaae18774555143b00da88e9
a-wessels <aydanwessels@gmail.com>
2023-05-22 13:15:28 -0600

    Tabs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,5 @@
 console.log('hello');
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var options = {
-    edge: "left"
-  };
-  var instances = M.Sidenav.init(elems, options);
-});
+
+
